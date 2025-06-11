47da4401393dd26e314accafd9e1cb734c48ae22
TAKAHASHI_KURUMI <renrakuyoutakahashi@gmail.com>
2025-06-11 13:55:09 +0900

    狼の目の挙動。ポッキーPrefab設定。

--- Assets/MyAssets/Scripts/MIniGame01/WolfEyesGenerator.cs
+++ Assets/MyAssets/Scripts/MIniGame01/WolfEyesGenerator.cs
@@ -3,8 +3,8 @@ using UnityEngine.InputSystem;
 
 public class WolfEyesGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject wolfEyesPrefab;
-    public float span;
+    [SerializeField] private GameObject wolfEyeRightPrefab;
+    [SerializeField] private GameObject wolfEyeLeftPrefab;
     [SerializeField] private float delta = 0f;
     [SerializeField] private float timer = 0f;
     [SerializeField] private int count = 0;
@@ -16,21 +16,50 @@ public class WolfEyesGenerator : MonoBehaviour
         if (this.timer < 30f)
         {
 
-            if (this.delta > span && count%2 ==0)
+            if (this.delta > 3 && count == 0)
             {
-                this.delta = 0;
-                GameObject go = Instantiate(wolfEyesPrefab);
-                int px = Random.Range(-8, 0);
-                go.transform.position = new Vector3(px, 3, 0);
+                count += 1;
+                GameObject go = Instantiate(wolfEyeRightPrefab);
+                int px = Random.Range(-7, 0);
+                go.transform.position = new Vector3(px, 2, 0);
             }
-            else if(this.delta > span && count % 2 == 1)
+            else if(this.delta >8  && count == 1)
             {
-                this.delta = 0;
-                GameObject go = Instantiate(wolfEyesPrefab);
-                transform.localScale = new Vector3(-1, 1, 1);
-                int px = Random.Range(0, 9);
-                go.transform.position = new Vector3(px, 3, 0);
+                count += 1;
+                GameObject go = Instantiate(wolfEyeLeftPrefab);
+                int px = Random.Range(0, 8);
+                go.transform.position = new Vector3(px, 2, 0);
             }
+            else if (this.delta > 12 && count == 2)
+            {
+                count += 1;
+                GameObject go = Instantiate(wolfEyeRightPrefab);
+                int px = Random.Range(-7,0);
+                go.transform.position = new Vector3(px, 2, 0);
+            }
+            else if (this.delta > 16 && count == 3)
+            {
+                count += 1;
+                GameObject go = Instantiate(wolfEyeLeftPrefab);
+                int px = Random.Range(0, 8);
+                go.transform.position = new Vector3(px, 2, 0);
+            }
+            else if (this.delta > 20 && count == 4)
+            {
+                count += 1;
+                GameObject go = Instantiate(wolfEyeRightPrefab);
+                int px = Random.Range(-7,0);
+                go.transform.position = new Vector3(px, 2, 0);
+            }
+            else if (this.delta > 25 && count == 5)
+            {
+                count += 1;
+                GameObject go1 = Instantiate(wolfEyeRightPrefab);
+                GameObject go2 = Instantiate(wolfEyeLeftPrefab);
+                go1.transform.position = new Vector3(-5, 2, 0);
+                go2.transform.position = new Vector3(5, 2, 0);
+            }
+            
         }
     }
 }
