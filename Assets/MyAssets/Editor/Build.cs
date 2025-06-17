d19127934f079503a8558a745f29008dad7cddc2
TAKAHASHI_KURUMI <renrakuyoutakahashi@gmail.com>
2025-06-17 10:41:36 +0900

    Bild

--- Assets/MyAssets/Editor/Build.cs
+++ Assets/MyAssets/Editor/Build.cs
@@ -1,19 +1,19 @@
 using UnityEditor;
 
-public static class BuildApp
+public static class BuildApp 
 {
     [MenuItem("Build/BuildApp")]
     public static void Build()
     {
         //windows64のプラットフォームでアプリをビルドする
         BuildPipeline.BuildPlayer(
-            new string[] {
-                 "Assets/MyAssets/Scenes/TitleScene.unity",
-                "Assets/MyAssets/Scenes/AthleticScene01.unity" ,
-                 "Assets/MyAssets/Scenes/MiniGameScene01.unity",
-                "Assets/MyAssets/Scenes/AthleticScene02.unity" ,
-                 "Assets/MyAssets/Scenes/MiniGameScene02.unity" ,
-                "Assets/MyAssets/Scenes/GameOverScene.unity"
+            new string[] { 
+                 "Assets/Scenes/TitleScene.unity",
+                "Assets/Scenes/AthleticScene01.unity" ,
+                 "Assets/Scenes/MiniGameScene01.unity",
+                "Assets/Scenes/AthleticScene02.unity" ,
+                 "Assets/Scenes/MiniGameScene02.unity" ,
+                "Assets/Scenes/GameOverScene.unity"
 
             },
             "Builds/App/OkashiRunGame.exe",
