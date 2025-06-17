46318f56fbc104e7239e12e78313c887ab9cd32d
maki_kaori <kaolyyn12ayamin20@gmail.com>
2025-06-17 15:21:57 +0900

    タイトル遷移時の修正
    (cherry picked from commit 4e9f87e1dc205862db90da7906a80505e0d053c1)

--- Assets/MyAssets/Scripts/Opening/StartController.cs
+++ Assets/MyAssets/Scripts/Opening/StartController.cs
@@ -1,17 +1,19 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
+
 
 public class StartController : MonoBehaviour
 {
     [SerializeField] private Button startButton;
-
+    [SerializeField] private TransitionManager transitionManager; // アニメーションPrefab
 
     private void Start()
     {
         SetEvent();
     }
-
     void SetEvent()
     {
         startButton.onClick.AddListener(() => StartGame());
@@ -20,7 +22,14 @@ public class StartController : MonoBehaviour
     public void StartGame()
     {
         Debug.Log("ボタン押されたよ");
+        WaitAnim().Forget();
+    }
+    async UniTask WaitAnim()
+    {
+        await transitionManager.SheepMaskOut();
+        // アニメーションが完了したらシーン遷移
         SceneManager.LoadScene("AthleticScene01");
+
     }
 
 }
